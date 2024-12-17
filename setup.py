1f6de9da0448ee616326a9512944510fbfba46d5
Matt Newsted <mnewsted@illinoislegalaid.org>
2024-12-17 10:12:53 -0600

    updated e-signature interview text to match forms

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.RespondToALawsuit',
-      version='1.1.16',
+      version='1.1.17',
       description=('Respond to a lawsuit'),
       long_description='# docassemble.RespondToALawsuit\r\n\r\nRespond to a lawsuit\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -52,9 +52,8 @@ setup(name='docassemble.RespondToALawsuit',
       author_email='mnewsted@illinoislegalaid.org',
       license='The MIT License (MIT)',
       url='https://www.illinoislegalaid.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=3.1.0', 'docassemble.ILAO>=1.0.99'],
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.ILAO>=1.0.99'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/RespondToALawsuit/', package='docassemble.RespondToALawsuit'),
      )
